f8bfb95cb9180f15fd476cefe6d2d0a8984bb56a
anhddabilive <anhdd@abilive.vn>
2026-06-20 21:40:09 +0700

    Stack Auth Init + add google font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
+import { inter } from '@/app/font';
+import { stackServerApp } from '@/stack/server';
+import { StackProvider, StackTheme } from '@stackframe/stack';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -23,8 +15,12 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-			<body className="min-h-full flex flex-col">{children}</body>
+		<html lang="en" className="h-full antialiased">
+			<body className={`${inter.className} min-h-full flex flex-col`}>
+				<StackProvider app={stackServerApp}>
+					<StackTheme>{children}</StackTheme>
+				</StackProvider>
+			</body>
 		</html>
 	);
 }
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from '@stackframe/stack';
+
+export default function Page() {
+	return (
+		<div className="flex items-center justify-center h-screen">
+			<SignIn />
+		</div>
+	);
+}
